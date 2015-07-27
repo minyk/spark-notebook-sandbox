6d0d06e32e6f2b6badffa529f221164d094aa248
minyk <minykreva@gmail.com>
2015-07-27 22:50:22 +0900

    Change the value of `HADOOP_CONF_DIR` to `/usr/local/hadoop/etc/hadoop` #4

--- resources/spark-notebook/spark-notebook.sh
+++ resources/spark-notebook/spark-notebook.sh
@@ -1,4 +1,4 @@
-export HADOOP_CONF_DIR=/usr/local/hadoop/conf
+export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop
 export EXTRA_CLASSPATH=/usr/local/hadoop/share/hadoop/common/lib/hadoop-lzo.jar:/home/hadoop/hive/conf
 export SPARKNOTEBOOK_HOME=/usr/local/spark-notebook
 export PATH=${SPARKNOTEBOOK_HOME}/bin:${PATH}
