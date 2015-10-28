f4c7b5b5e7730e3626c6ff9052edf3302b1d4b2c
minyk <minykreva@gmail.com>
2015-10-28 16:45:10 +0900

    Refactor start script

--- scripts/init-start-all-services.sh
+++ scripts/init-start-all-services.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 source "/vagrant/scripts/common.sh"
+source "/vagrant/scripts/start-all-services.sh"
 
 function formatNameNode {
 	$HADOOP_PREFIX/bin/hdfs namenode -format myhadoop -force -noninteractive
 	echo "formatted namenode"
 }
 
-function startHDFS {
-	$HADOOP_PREFIX/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode
-	$HADOOP_PREFIX/sbin/hadoop-daemons.sh --config $HADOOP_CONF_DIR --script hdfs start datanode
-	echo "started hdfs"
-}
-
-function startYarn {
-	$HADOOP_YARN_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager
-	$HADOOP_YARN_HOME/sbin/yarn-daemons.sh --config $HADOOP_CONF_DIR start nodemanager
-	$HADOOP_YARN_HOME/sbin/yarn-daemon.sh start proxyserver --config $HADOOP_CONF_DIR
-	$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver --config $HADOOP_CONF_DIR
-	echo "started yarn"
-}
+#function startHDFS {
+#	$HADOOP_PREFIX/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode
+#	$HADOOP_PREFIX/sbin/hadoop-daemons.sh --config $HADOOP_CONF_DIR --script hdfs start datanode
+#	echo "started hdfs"
+#}
+#
+#function startYarn {
+#	$HADOOP_YARN_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager
+#	$HADOOP_YARN_HOME/sbin/yarn-daemons.sh --config $HADOOP_CONF_DIR start nodemanager
+#	$HADOOP_YARN_HOME/sbin/yarn-daemon.sh start proxyserver --config $HADOOP_CONF_DIR
+#	$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver --config $HADOOP_CONF_DIR
+#	echo "started yarn"
+#}
 
 function createEventLogDir {
 	$HADOOP_PREFIX/bin/hdfs dfs -mkdir /tmp
@@ -26,16 +27,16 @@ function createEventLogDir {
 	echo "created spark event log dir"
 }
 
-function startSpark {
-	$SPARK_HOME/sbin/start-all.sh
-	$SPARK_HOME/sbin/start-history-server.sh
-	echo "started spark"
-}
-
-function startSparknotebook {
-    $SPARKNOTEBOOK_HOME/start-spark-notebook.sh
-    echo "started spark-notebook"
-}
+#function startSpark {
+#	$SPARK_HOME/sbin/start-all.sh
+#	$SPARK_HOME/sbin/start-history-server.sh
+#	echo "started spark"
+#}
+#
+#function startSparknotebook {
+#    $SPARKNOTEBOOK_HOME/start-spark-notebook.sh
+#    echo "started spark-notebook"
+#}
 
 function setupServices {
     cp -f /vagrant/scripts/start-all-services.sh /etc/init.d/start-all-services
@@ -44,9 +45,9 @@ function setupServices {
 }
 
 formatNameNode
-startHDFS
+start_hdfs
 #startYarn
 createEventLogDir
-startSpark
-startSparknotebook
+start_spark
+start_sparknotebook
 setupServices
